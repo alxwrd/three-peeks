9092c6994cfc7d41c76822f2047bd6d2f315ffe3
Alex Ward <alxwrd@googlemail.com>
2020-02-26 22:50:12 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Three Peek Challenge
+
+## Build
+
+```
+npm install --only=dev
+npm run build
+```
---
+++ src/App.svelte
@@ -0,0 +1,54 @@
+<script>
+
+</script>
+
+<header class="bg-gray-900 h-16 flex shadow-md">
+  <div class="w-1/4 p-4 md:hidden cursor-pointer">
+    <svg
+      class="fill-current text-white h-8 w-8"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20">
+      <path
+        d="M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0
+        .6-.447.6-1 0-.553-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1
+        .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zM3.6 7h12.8c.552 0
+        .6-.447.6-1 0-.553-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6
+        1z" />
+    </svg>
+  </div>
+
+
+  <div class="hidden md:flex w-full align-middle">
+    <img src="images/logo.png" class="w-12 h-8 m-4" />
+    <div
+      class="w-32 m-5 text-gray-400 cursor-pointer hover:font-extrabold
+      text-center">
+      The Climbers
+    </div>
+    <div
+      class="w-32 m-5 text-gray-400 cursor-pointer hover:font-extrabold
+      text-center">
+      The Routes
+    </div>
+    <div
+      class="w-32 m-5 text-gray-400 cursor-pointer hover:font-extrabold
+      text-center">
+      The Mission
+    </div>
+  </div>
+</header>
+
+<main class="h-100">
+
+  <div
+    class="text-4xl md:text-6xl text-gray-400 text-center pt-32 pb-64 font-bold"
+    style="font-family: 'Playfair Display SC';">
+    Dare to peek
+    <div
+      class="text-sm md:text-lg font-normal"
+      style="font-family: 'Montserrat';">
+      3 men, 3 peaks, 3 peeks, 1 dream
+    </div>
+  </div>
+
+</main>
